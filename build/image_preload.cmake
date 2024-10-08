6ae0e599407643bb1cf6421c4a4c9cdbd1d8a18c
avijay-1 <avijay@wearenova.co>
2024-10-08 13:09:56 -0400

    Delete build directory

--- build/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf5340dk_nrf5340_cpuapp_ns" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "c:/untitled;C:/ncs/v2.6.1/nrf;C:/ncs/v2.6.1/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "c:/untitled/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "boards/nrf5340dk_nrf5340_cpuapp_ns.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/ce3b5ff664/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
